028a08bb71275ccf09c1178ff65e3a4fb9834ebf
yunhes45 <yunhes45@gmail.com>
2022-01-28 22:23:34 +0900

    first commit

---
+++ src/main/java/com/example/mychatproj/MychatprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.mychatproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MychatprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MychatprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mychatproj/MychatprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mychatproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MychatprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
